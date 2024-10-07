b4eaaf2580bb0c48df80767fc2b102ee776fc5cc
giorgio <giorgio.ngero@mail.polimi.it>
2024-10-07 18:40:38 +0200

    Tensor draft, cmake

---
+++ include/Tensor.h
@@ -0,0 +1,23 @@
+//
+// Created by giorgio on 07/10/24.
+//
+#include <vector>
+
+#ifndef MPI_INCOMPRESSIBLE_FLUID_TENSOR_H
+#define MPI_INCOMPRESSIBLE_FLUID_TENSOR_H
+
+namespace mif {
+
+    class Tensor {
+    private:
+        std::vector<double> data;
+    public:
+        template<typename F>
+            void set(F lambda);
+
+    };
+
+
+} // mif
+
+#endif //MPI_INCOMPRESSIBLE_FLUID_TENSOR_H
---
+++ src/Tensor.cpp
@@ -0,0 +1,9 @@
+//
+// Created by giorgio on 07/10/24.
+//
+
+#include "../include/Tensor.h"
+
+namespace mif {
+
+}
