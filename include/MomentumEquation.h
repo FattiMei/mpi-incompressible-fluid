b02f3252173fc2247babc2df6d5fd9cbcd3fbd23
giorgio <giorgionegro@proton.me>
2024-10-10 15:23:22 +0200

    timestep function

--- include/MomentumEquation.h
+++ include/MomentumEquation.h
@@ -14,7 +14,7 @@
 // TODO: Assess wether or not using the `get` method of the Tensor class has an impact on performance with
 // respect to accessing the data directly.
 
-namespace mif 
+namespace mif
 {
 
 // The momentum equation is composed by two terms: convection and diffusion.
@@ -33,25 +33,25 @@ inline Real calculate_momentum_rhs_u(const Tensor &u, const Tensor &v, const Ten
             // TERM: u ∂u/∂x
             // We use a first-order finite difference scheme to approximate the convection term,
             // this is sufficient for second-order accuracy in the overall scheme.
-            - u.get(i, j, k) * 
+            - u.get(i, j, k) *
             (u.get(i+1, j, k) - u.get(i-1, j, k)) * constants.one_over_2_dx
 
             // TERM: v ∂u/∂y
             // We use a second-order central difference scheme to approximate the convection term.
-            -(v.get(i, j, k) + v.get(i, j-1, k) + v.get(i+1, j, k) + v.get(i+1, j-1, k)) * 
+            -(v.get(i, j, k) + v.get(i, j-1, k) + v.get(i+1, j, k) + v.get(i+1, j-1, k)) *
              (u.get(i, j+1, k) - u.get(i, j-1, k)) * constants.one_over_8_dy
-            
+
             // TERM: w ∂u/∂z
-            -(w.get(i, j, k) + w.get(i, j, k-1) + w.get(i+1, j, k) + w.get(i+1, j, k-1)) * 
+            -(w.get(i, j, k) + w.get(i, j, k-1) + w.get(i+1, j, k) + w.get(i+1, j, k-1)) *
              (u.get(i, j, k+1) - u.get(i, j, k-1)) * constants.one_over_8_dz;
 
-    const Real diffusion_term = 
+    const Real diffusion_term =
         // TERM: 1/Re * (∂²u/∂x² + ∂²u/∂y² + ∂²u/∂z²)
         // We use a second-order central difference scheme to approximate the second derivatives.
         (u.get(i+1, j, k) - 2 * u.get(i, j, k) + u.get(i-1, j, k)) * constants.one_over_dx2_Re +
         (u.get(i, j+1, k) - 2 * u.get(i, j, k) + u.get(i, j-1, k)) * constants.one_over_dy2_Re +
         (u.get(i, j, k+1) - 2 * u.get(i, j, k) + u.get(i, j, k-1)) * constants.one_over_dz2_Re;
-    
+
     return convection_term + diffusion_term;
 }
 
@@ -60,19 +60,19 @@ inline Real calculate_momentum_rhs_u(const Tensor &u, const Tensor &v, const Ten
 inline Real calculate_momentum_rhs_v(const Tensor &u, const Tensor &v, const Tensor &w, // Velocity field.
                                      const size_t i, const size_t j, const size_t k,    // Grid point.
                                      const Constants &constants) {
-    const Real convection_term = 
+    const Real convection_term =
             // TERM: u ∂v/∂x
             -(u.get(i, j, k) + u.get(i-1, j, k) + u.get(i, j+1, k) + u.get(i-1, j+1, k)) *
              (v.get(i+1, j, k) - v.get(i-1, j, k)) * constants.one_over_8_dx
-            
+
             // TERM: v ∂v/∂y
             - v.get(i, j, k) *
              (v.get(i, j+1, k) - v.get(i, j-1, k)) * constants.one_over_2_dy
-            
+
             // TERM: w ∂v/∂z
             -(w.get(i, j, k) + w.get(i, j, k-1) + w.get(i, j+1, k) + w.get(i, j+1, k-1)) *
              (v.get(i, j, k+1) - v.get(i, j, k-1)) * constants.one_over_8_dz;
-    
+
     const Real diffusion_term =
             // TERM: 1/Re * (∂²v/∂x² + ∂²v/∂y² + ∂²v/∂z²)
             (v.get(i+1, j, k) - 2 * v.get(i, j, k) + v.get(i-1, j, k)) * constants.one_over_dx2_Re +
@@ -87,7 +87,7 @@ inline Real calculate_momentum_rhs_v(const Tensor &u, const Tensor &v, const Ten
 inline Real calculate_momentum_rhs_w(const Tensor &u, const Tensor &v, const Tensor &w, // Velocity field.
                                      const size_t i, const size_t j, const size_t k,    // Grid point.
                                      const Constants &constants) {
-    const Real convection_term = 
+    const Real convection_term =
             // TERM: u ∂w/∂x
             -(u.get(i, j, k) + u.get(i-1, j, k) + u.get(i, j, k+1) + u.get(i-1, j, k+1)) *
              (w.get(i+1, j, k) - w.get(i-1, j, k)) * constants.one_over_8_dx
@@ -95,11 +95,11 @@ inline Real calculate_momentum_rhs_w(const Tensor &u, const Tensor &v, const Ten
             // TERM: v ∂w/∂y
             -(v.get(i, j, k) + v.get(i, j-1, k) + v.get(i, j, k+1) + v.get(i, j-1, k+1)) *
              (w.get(i, j+1, k) - w.get(i, j-1, k)) * constants.one_over_8_dy
-            
+
             // TERM: w ∂w/∂z
             - w.get(i, j, k) * (w.get(i, j, k+1) - w.get(i, j, k-1)) * constants.one_over_2_dz;
-    
-    const Real diffusion_term = 
+
+    const Real diffusion_term =
         // TERM: 1/Re * (∂²w/∂x² + ∂²w/∂y² + ∂²w/∂z²)
         (w.get(i+1, j, k) - 2 * w.get(i, j, k) + w.get(i-1, j, k)) * constants.one_over_dx2_Re +
         (w.get(i, j+1, k) - 2 * w.get(i, j, k) + w.get(i, j-1, k)) * constants.one_over_dy2_Re +
