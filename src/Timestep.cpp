257f93f2fd53a2c0d9ae2a9245bf8f84d16edaa0
FrancescoPesce <francesco1.pesce@mail.polimi.it>
2024-10-12 15:44:44 +0200

    Fix bugs in timestep method

--- src/Timestep.cpp
+++ src/Timestep.cpp
@@ -2,6 +2,7 @@
 // Created by giorgio on 10/10/2024.
 //
 
+#include <cassert>
 #include <functional>
 #include "Constants.h"
 #include "MomentumEquation.h"
@@ -10,27 +11,65 @@
 
 namespace mif {
 
+    enum VelocityComponent {u, v, w};
+
     // Perform the first stage of a single step of an explicit RK3 method for a single point of a single component.
-    inline Real
-    compute_Y2(const Tensor<> &u, Real dt, const std::function<Real(size_t, size_t, size_t, Real)> &f, size_t i, size_t j, size_t k) {
+    template <VelocityComponent component> inline Real
+    compute_Y2(const Tensor<> &u, const Tensor<> &v, const Tensor<> &w, 
+               const Constants &constants, size_t i, size_t j, size_t k) {
         constexpr Real a1 = (64.0 / 120.0);
-        return u(i,j,k) + dt * f(i, j, k, u(i,j,k)) * a1;
+        switch (component)
+        {
+        case VelocityComponent::u:
+            return u(i,j,k) + constants.dt * calculate_momentum_rhs_u(u, v, w, i, j, k, constants) * a1;
+        case VelocityComponent::v:
+            return v(i,j,k) + constants.dt * calculate_momentum_rhs_v(u, v, w, i, j, k, constants) * a1;
+        case VelocityComponent::w:
+            return w(i,j,k) + constants.dt * calculate_momentum_rhs_w(u, v, w, i, j, k, constants) * a1;
+        default:
+            assert(false);
+        }
     }
 
     // Perform the second stage of a single step of an explicit RK3 method for a single point of a single component.
-    inline Real 
-    compute_Y3(const Tensor<> &u, const Tensor<> &Y2, Real dt, const std::function<Real(size_t, size_t, size_t, Real)> &f, size_t i, size_t j, size_t k) {
+    template <VelocityComponent component> inline Real
+    compute_Y3(const Tensor<> &u, const Tensor<> &v, const Tensor<> &w, 
+               const Tensor<> &Y2_u, const Tensor<> &Y2_v, const Tensor<> &Y2_w,
+               const Constants &constants, size_t i, size_t j, size_t k) {
         constexpr Real a2 = (50.0 / 120.0);
         constexpr Real a3 = (-34.0 / 120.0);
-        return Y2(i,j,k) + dt * a2 * f(i, j, k, Y2(i,j,k)) + dt * a3 * f(i, j, k, u(i,j,k));
+        switch (component)
+        {
+        case VelocityComponent::u:
+            return Y2_u(i,j,k) + constants.dt * a2 * calculate_momentum_rhs_u(Y2_u, Y2_v, Y2_w, i, j, k, constants) + constants.dt * a3 * calculate_momentum_rhs_u(u, v, w, i, j, k, constants);
+        case VelocityComponent::v:
+            return Y2_v(i,j,k) + constants.dt * a2 * calculate_momentum_rhs_v(Y2_u, Y2_v, Y2_w, i, j, k, constants) + constants.dt * a3 * calculate_momentum_rhs_v(u, v, w, i, j, k, constants);
+        case VelocityComponent::w:
+            return Y2_w(i,j,k) + constants.dt * a2 * calculate_momentum_rhs_w(Y2_u, Y2_v, Y2_w, i, j, k, constants) + constants.dt * a3 * calculate_momentum_rhs_w(u, v, w, i, j, k, constants);
+        default:
+            assert(false);
+        }
     }
 
     // Perform the third stage of a single step of an explicit RK3 method for a single point of a single component.
-    inline Real 
-    compute_new_u(const Tensor<> &u, const Tensor<> &Y2, const Tensor<> &Y3, Real dt, const std::function<Real(size_t, size_t, size_t, Real)> &f, size_t i, size_t j, size_t k) {
+    template <VelocityComponent component> inline Real
+    compute_new_u(const Tensor<> &u, const Tensor<> &v, const Tensor<> &w, 
+               const Tensor<> &Y2_u, const Tensor<> &Y2_v, const Tensor<> &Y2_w,
+               const Tensor<> &Y3_u, const Tensor<> &Y3_v, const Tensor<> &Y3_w,
+               const Constants &constants, size_t i, size_t j, size_t k) {
         constexpr Real a4 = (90.0 / 120.0);
         constexpr Real a5 = (-50.0 / 120.0);
-        return Y2(i,j,k) + dt * a5 * f(i, j, k, u(i,j,k)) + dt * a4 * f(i, j, k, Y3(i,j,k));
+        switch (component)
+        {
+        case VelocityComponent::u:
+            return Y2_u(i,j,k) + constants.dt * a5 * calculate_momentum_rhs_u(u, v, w, i, j, k, constants) + constants.dt * a4 * calculate_momentum_rhs_u(Y3_u, Y3_v, Y3_w, i, j, k, constants);
+        case VelocityComponent::v:
+            return Y2_v(i,j,k) + constants.dt * a5 * calculate_momentum_rhs_v(u, v, w, i, j, k, constants) + constants.dt * a4 * calculate_momentum_rhs_v(Y3_u, Y3_v, Y3_w, i, j, k, constants);
+        case VelocityComponent::w:
+            return Y2_w(i,j,k) + constants.dt * a5 * calculate_momentum_rhs_w(u, v, w, i, j, k, constants) + constants.dt * a4 * calculate_momentum_rhs_w(Y3_u, Y3_v, Y3_w, i, j, k, constants);
+        default:
+            assert(false);
+        }
     }
 
     void timestep(Tensor<> &u, Tensor<> &v, Tensor<> &w, 
@@ -38,45 +77,24 @@ namespace mif {
                   Tensor<> &u_buffer2, Tensor<> &v_buffer2, Tensor<> &w_buffer2,  
                   Tensor<> &u_buffer3, Tensor<> &v_buffer3, Tensor<> &w_buffer3,  
                   const Constants &constants) {
-        auto f_u = [&u, &v, &w, &constants](size_t i, size_t j, size_t k, Real x) {
-            return calculate_momentum_rhs_u(u, v, w, i, j, k, constants);
-        };
-        auto f_v = [&u, &v, &w, &constants](size_t i, size_t j, size_t k, Real x) {
-            return calculate_momentum_rhs_v(u, v, w, i, j, k, constants);
-        };
-        auto f_w = [&u, &v, &w, &constants](size_t i, size_t j, size_t k, Real x) {
-            return calculate_momentum_rhs_w(u, v, w, i, j, k, constants);
-        };
-
-        // Update the velocity solution inside the mesh - stage 1.
+        // Update the velocity solution inside the mesh.
         for (size_t i = 1; i < constants.Nx - 1; i++) {
             for (size_t j = 1; j < constants.Ny - 1; j++) {
                 for (size_t k = 1; k < constants.Nz - 1; k++) {
-                    u_buffer1(i, j, k) = compute_Y2(u, constants.dt, f_u, i, j, k);
-                    v_buffer1(i, j, k) = compute_Y2(v, constants.dt, f_v, i, j, k);
-                    w_buffer1(i, j, k) = compute_Y2(w, constants.dt, f_w, i, j, k);
-                }
-            }
-        }
+                    // Stage 1.
+                    u_buffer1(i, j, k) = compute_Y2<VelocityComponent::u>(u, v, w, constants, i, j, k);
+                    v_buffer1(i, j, k) = compute_Y2<VelocityComponent::v>(u, v, w, constants, i, j, k);
+                    w_buffer1(i, j, k) = compute_Y2<VelocityComponent::w>(u, v, w, constants, i, j, k);
 
-        // Update the velocity solution inside the mesh - stage 2.
-        for (size_t i = 1; i < constants.Nx - 1; i++) {
-            for (size_t j = 1; j < constants.Ny - 1; j++) {
-                for (size_t k = 1; k < constants.Nz - 1; k++) {
-                    u_buffer2(i, j, k) = compute_Y3(u, u_buffer1, constants.dt, f_u, i, j, k);
-                    v_buffer2(i, j, k) = compute_Y3(v, v_buffer1, constants.dt, f_v, i, j, k);
-                    w_buffer2(i, j, k) = compute_Y3(w, w_buffer1, constants.dt, f_w, i, j, k);
-                }
-            }
-        }
+                    // Stage 2.
+                    u_buffer2(i, j, k) = compute_Y3<VelocityComponent::u>(u, v, w, u_buffer1, v_buffer1, w_buffer1, constants, i, j, k);
+                    v_buffer2(i, j, k) = compute_Y3<VelocityComponent::v>(u, v, w, u_buffer1, v_buffer1, w_buffer1, constants, i, j, k);
+                    w_buffer2(i, j, k) = compute_Y3<VelocityComponent::w>(u, v, w, u_buffer1, v_buffer1, w_buffer1, constants, i, j, k);
 
-        // Update the velocity solution inside the mesh - stage 3.
-        for (size_t i = 1; i < constants.Nx - 1; i++) {
-            for (size_t j = 1; j < constants.Ny - 1; j++) {
-                for (size_t k = 1; k < constants.Nz - 1; k++) {
-                    u_buffer3(i, j, k) = compute_new_u(u, u_buffer1, u_buffer2, constants.dt, f_u, i, j, k);
-                    v_buffer3(i, j, k) = compute_new_u(v, v_buffer1, v_buffer2, constants.dt, f_v, i, j, k);
-                    w_buffer3(i, j, k) = compute_new_u(w, w_buffer1, w_buffer2, constants.dt, f_w, i, j, k);
+                    // Stage 3.
+                    u_buffer3(i, j, k) = compute_new_u<VelocityComponent::u>(u, v, w, u_buffer1, v_buffer1, w_buffer1, u_buffer2, v_buffer2, w_buffer2, constants, i, j, k);
+                    v_buffer3(i, j, k) = compute_new_u<VelocityComponent::v>(u, v, w, u_buffer1, v_buffer1, w_buffer1, u_buffer2, v_buffer2, w_buffer2, constants, i, j, k);
+                    w_buffer3(i, j, k) = compute_new_u<VelocityComponent::w>(u, v, w, u_buffer1, v_buffer1, w_buffer1, u_buffer2, v_buffer2, w_buffer2, constants, i, j, k);
                 }
             }
         }
