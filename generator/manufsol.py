529ab92f35b3cfccc55011862235bcc9af5362a8
Michele Miotti <michele.miotti@mail.polimi.it>
2024-10-29 11:14:44 +0100

    Add documentation to manufsol.py

--- generator/manufsol.py
+++ generator/manufsol.py
@@ -1,42 +1,65 @@
+"""
+This script generates the C++ code for a given manufactured solution.
+It uses the sympy library to symbolically manipulate the expressions and
+generate the code through the codegen utility. The produced code is then
+printed to the standard output.
+
+See FORCING_TERM_GENERATOR and FORCING_TERM_ARTIFACT from the CMakelists.txt
+file for more information on how this script is used in the project.
+"""
+
+
 import sys
 import sympy as sp
 from sympy import diff
+from sympy import simplify as simp
+
+# The codegen callable is not in the sympy namespace automatically.
 from sympy.utilities.codegen import codegen
 
 
-t, x, y, z, Re = sp.symbols('t x y z Reynolds')
+# Create the required symbols.
+t, x, y, z, Re = sp.symbols('t, x, y, z, Reynolds')
 
 
-def laplacian(u):
+# Define the laplacian operator.
+def lap(u):
     return diff(u,x,2) + diff(u,y,2) + diff(u,z,2)
 
-
+# Define the manufactured solution function.
+# Note: the asterisk (*) in the function signature indicates that the arguments
+# are keyword-only. This is to minimize the risk of passing wrong arguments.
 def manufsol(*, u, v, w, p, ignore_pressure=False):
-    if diff(u,x) + diff(v,y) + diff(w,z) != 0:
-        sys.stderr.write('[WARNING]: the proposed manufactured solution is not divergence free, correcting\n')
-        w = sp.integrate(sp.simplify( sp.diff(u, x) + sp.diff(v, y) ), z)
-
     if ignore_pressure:
         p = 0
+    
+    # Safety check: the proposed manufactured solution must be divergence free.
+    if diff(u,x) + diff(v,y) + diff(w,z) != 0:
+        w = sp.integrate(simp(sp.diff(u, x) + sp.diff(v, y)), z)
+        sys.stderr.write('[WARNING]: the proposed manufactured solution was \
+                         not divergence free, so the w term was overridden.\n')
 
-    fx = sp.simplify( diff(u,t) + u*diff(u,x) + v*diff(u,y) + w*diff(u,z) + diff(p,x) - laplacian(u)/Re )
-    fy = sp.simplify( diff(v,t) + u*diff(v,x) + v*diff(v,y) + w*diff(v,z) + diff(p,y) - laplacian(v)/Re )
-    fz = sp.simplify( diff(w,t) + u*diff(w,x) + v*diff(w,y) + w*diff(w,z) + diff(p,z) - laplacian(w)/Re )
+    fx = simp(diff(u,t) + u*diff(u,x) + v*diff(u,y) + w*diff(u,z) + diff(p,x) - lap(u)/Re)
+    fy = simp(diff(v,t) + u*diff(v,x) + v*diff(v,y) + w*diff(v,z) + diff(p,y) - lap(v)/Re)
+    fz = simp(diff(w,t) + u*diff(w,x) + v*diff(w,y) + w*diff(w,z) + diff(p,z) - lap(w)/Re)
 
     return (u,v,w), (fx,fy,fz)
 
 
 if __name__ == '__main__':
-    u = sp.sin(x)*sp.cos(y)*sp.sin(z)*sp.sin(t)
-    v = sp.cos(x)*sp.sin(y)*sp.sin(z)*sp.sin(t)
+    # This is the manufactured solution we want to generate the code for.
+    u =   sp.sin(x)*sp.cos(y)*sp.sin(z)*sp.sin(t)
+    v =   sp.cos(x)*sp.sin(y)*sp.sin(z)*sp.sin(t)
     w = 2*sp.cos(x)*sp.cos(y)*sp.cos(z)*sp.sin(t)
 
-    # don't set the pressure to zero or constant otherwise the generated function signature will be screwed
+    # WARNING: this shouldn't be a constant value, otherwise the codegen will
+    # generate a wrong function signature. Use the ignore_pressure flag instead.
     p = sp.sin(t)*x*y*z
 
-    # for current testing purpuoses we ignore the pressure term
+    # Until we tackle the pressure term, we can ignore it.
     (u,v,w), (fx,fy,fz) = manufsol(u=u,v=v,w=w,p=p,ignore_pressure=True)
 
+    # Generate the C code through sympy's codegen utility.
     [(c_name, c_code), (h_name, c_header)] = codegen(
         [
             ('u_exact'  ,  u),
@@ -56,4 +79,5 @@ if __name__ == '__main__':
         global_vars=[Re]
     )
 
+    # This code will be fed to the `manufsol.cpp` file.
     print(c_code)
