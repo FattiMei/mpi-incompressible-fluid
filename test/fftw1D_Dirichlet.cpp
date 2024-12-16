d832c16bac9864fcdc5a68f42d8bb41f2811cc15
FattiMei <teomei68@gmail.com>
2024-12-16 17:27:56 +0100

    bugfix: remove last element in solution, now reaches 2nd order convergence

--- test/fftw1D_Dirichlet.cpp
+++ test/fftw1D_Dirichlet.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 // This test the FFT method to solve the problem Ax=b
 // @MatteoLeone wants to solve a real problem Laplacian(p) = f
-int main(int argc, char *argv[]) {
+int main() {
 	const int N = std::atoi(argv[1]);
 
 	double *x      = (double*) fftw_malloc(sizeof(double) * N);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 	fftw_plan b_to_btilde_plan = fftw_plan_r2r_1d(N, b, btilde, FFTW_R2HC,  FFTW_ESTIMATE);
 	fftw_plan xtilde_to_x_plan = fftw_plan_r2r_1d(N, xtilde, x, FFTW_HC2R, FFTW_ESTIMATE);
 
-	const double h = 2.0*PI/(N-1);
+	const double h = 2.0*PI/(N);
 	for (int i = 0; i < N; ++i) {
 		xex[i] = std::cos(h*i);
 		b[i]   = -xex[i];
@@ -36,7 +36,6 @@ int main(int argc, char *argv[]) {
 	xtilde[0] = 0.0;
 
 	for (int i = 1; i < N; ++i) {
-		// ?
 		const double eig = 2*std::cos(2 * M_PI * i / N) - 2.0;
 
 		xtilde[i] = (btilde[i] / eig) * (h * h);
@@ -47,21 +46,13 @@ int main(int argc, char *argv[]) {
 
 	// ATTENTION! Renormalization is necessary such that IFFT . FFT == Identity
 	for (int i = 0; i < N; ++i) {
-		x[i] /= (N-1);
+		x[i] /= N;
 	}
 
 	const double constant = xex[0] - x[0];
-	double maxerr = 0.0;
 
 	for (int i = 0; i < N; ++i) {
-		const double err = std::abs(xex[i] - x[i]);
-
-		if (err > maxerr) {
-			maxerr = err;
-		}
-
-		// std::cout << x[i] << ' ';
-		// assert(std::abs(xex[i] - x[i] - constant) < 1e-6);
+		assert(std::abs(xex[i] - x[i] - constant) < 1e-6);
 	}
 	std::cout << maxerr << std::endl;
 
