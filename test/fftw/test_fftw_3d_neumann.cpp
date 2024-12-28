dbffc78bf0cd7a390077c4dda3ba45d89e3ef2c4
matteodeb <matteo.leone01@gmail.com>
2024-12-28 22:03:14 +0100

    First steps towards a more generalized solver

--- test/fftw/test_fftw_3d_neumann.cpp
+++ test/fftw/test_fftw_3d_neumann.cpp
@@ -1,3 +1,10 @@
+//TODO 
+// 
+// Initialize the rhs in order to match the domain decomp (Xstart ?)
+// Define size local for each pencil
+// Understand how to implement in order to avoid the x direction to be the slowest (Won't need exxtract array?)
+// use c2d method to allocX x, b, xtilde, ...
+
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -15,12 +22,9 @@ constexpr double PI = 3.141592653589793;
 using namespace std;
 
 inline double compute_eigenvalue_neumann(int index, int N) {
-	return (2.0 *cos( PI * index / (N-1)) - 2.0);
+    return (2.0 *cos( PI * index / (N-1)) - 2.0);
 }
 
-inline int index3D(int i, int j, int k, int N) {
-    return (i * N * N) + (j * N) + k;
-}
 
 void extract_array(double arr[], int size, int start, double subarray[]){
     // Copy the elements manually
@@ -49,42 +53,64 @@ int main(int argc, char *argv[]) {
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
 
 
+    // pRow*pCol = totRank to work properly!!
+    //int pRow = totRank, pCol = 1;
+    int pRow =0 , pCol = 0;
+    // True for periodic
+    bool neumannBC[3] = {false, false, false};
 
-    if(!mpiRank){
-        // cout << endl;
-        // cout << "-------------------" << endl;
-    	// cout << " C2Decomp Testing " << endl;
-    	// cout << "-------------------" << endl;
-    	// cout << endl;
 
-    }
+    //Initialize 2decomp
+    C2Decomp *c2d = new C2Decomp(Nx, Ny, Nz, pRow, pCol, neumannBC);
 
-	int size = Nx * Ny * Nz;
+    int NxLoc = c2d->xSize[0];
+    int NyLoc = c2d->xSize[1];
+    int NzLoc = c2d->xSize[2];
+    auto index3D =[&](int i, int j, int k) {
+        return (k * NxLoc * NyLoc) + (j * NxLoc) + i;
+    };
+    int size = Nx * Ny * Nz;
 
-	double *Uex    = (double*) fftw_malloc(sizeof(double) * size);
-    double *b      = (double*) fftw_malloc(sizeof(double) * size);
+    double *Uex   ;
+    double *b     ;
+    // Vectors with local informations
+    c2d->allocX( Uex );
+    c2d->allocX( b );
+
+
+
     // Create forcing term manufactured from manufactured sulotion (u =-3cx*cy*cz, x, y, z = [0, 2pi])
-    double h = 2*PI/(N-1) ;
+    double hx = 2*PI/(Nx-1) ;
+    double hy = 2*PI/(Ny-1) ;
+    double hz = 2*PI/(Nz-1) ;
     for (int i = 0; i < Nx; i++) {
-        for (int j = 0; j < Ny; j++) {
-            for (int k = 0; k < Nz; k++) {
-                Uex[index3D(i,  j,  k,  N)] = std::cos(h*i) * std::cos(h*j) * std::cos(h*k) ;
-                b[index3D(i, j, k, N)] = -3.0*Uex[index3D(i,  j,  k,  N)];
+        for (int j = 0; j < NyLoc; j++) {
+            for (int k = 0; k < NzLoc; k++) {
+                int jGlobal = j + c2d->xStart[1], kGlobal = k+c2d->xStart[2];
+                Uex[index3D(i, j, k)] = std::cos(hx*i) * std::cos(hy*jGlobal) * std::cos(hz*kGlobal) ;
+                b[index3D(i, j, k)] = -3.0*Uex[index3D(i, j, k)];
             }    
         }
     }
-
-    // pRow*pCol = rankMPI to work properly!!
-    int pRow = 1, pCol = 1;
-    bool neumannBC[3] = {true, true, true};
-
-    //if(!mpiRank) cout << "initializing " << endl;
-    C2Decomp *c2d = new C2Decomp(N, N, N, pRow, pCol, neumannBC);
-    //if(!mpiRank) cout << "done initializing " << endl;
+    //Check riempimento
+    if(mpiRank==2){
+        cout << "y0" << c2d->xStart[1] << "\n"; 
+        cout << "z0" << c2d->xStart[2] << "\n"; 
+        for (int j = 0; j < NyLoc; j++) {
+            for (int k = 0; k < NzLoc; k++) {
+                for (int i = 0; i < Nx; i++) {
+                    int jGlobal = j + c2d->xStart[1], kGlobal = k+c2d->xStart[2];
+                    std::cout << Uex[index3D(i, j, k)] << " \t";
+                }    
+                cout << "\n";
+            }
+            cout << " New Plane \n";
+        }
+    }
 
     double *x      = (double*) fftw_malloc(sizeof(double) * size);
-	double *btilde = (double*) fftw_malloc(sizeof(double) * size);	
-	double *xtilde = (double*) fftw_malloc(sizeof(double) * size);
+    double *btilde = (double*) fftw_malloc(sizeof(double) * size);	
+    double *xtilde = (double*) fftw_malloc(sizeof(double) * size);
 
 
     double *temp1 = (double*) fftw_malloc(sizeof(double) * N);
@@ -97,13 +123,14 @@ int main(int argc, char *argv[]) {
             for (int j = 0; j < N; j++){
                 extract_array(b, N, i*N*N + j*N, temp1);
                 fftw_execute(b_to_btilde_plan);
-                for (int k = 0; k < N; k++)
-                    btilde[index3D(i, j, k,)] = temp2[k];
+                for (int k = 0; k < N; k++){
+                    btilde[index3D(i, j, k)] = temp2[k];
                 }
             }
         }
+        // TODO Funziona in parallelo che itero la rotazione xtoy??
         c2d->transposeX2Y_MajorIndex(btilde, btilde);
-            
+
         b = btilde;
     }
 
@@ -114,7 +141,7 @@ int main(int argc, char *argv[]) {
         for (int j = 0; j < N; j++){
             double t2= compute_eigenvalue_neumann(j, N);
             for (int k = 0; k < N; k++){
-                xtilde[index3D(i, j, k, N)] /= (t1 + t2 + compute_eigenvalue_neumann(k, N) )/std::pow(h, 2);
+                xtilde[index3D(i, j, k)] /= (t1 + t2 + compute_eigenvalue_neumann(k, N) )/std::pow(h, 2);
             }
         }
     }
@@ -126,10 +153,10 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++){
                 extract_array(xtilde, N, i*N*N + j*N, temp1);
-               
+
                 fftw_execute(xtilde_to_x_plan);
                 for (int k = 0; k < N; k++){
-                    x[index3D(i, j, k, N)] = ( temp2[k] )/( 2.0 * (N-1) ); // 
+                    x[index3D(i, j, k)] = ( temp2[k] )/( 2.0 * (N-1) ); // 
                 }
             }
         }
@@ -148,11 +175,11 @@ int main(int argc, char *argv[]) {
     cout<< mazx << endl;
 
     //Now lets kill MPI
-
     fftw_destroy_plan(b_to_btilde_plan);
     fftw_destroy_plan(xtilde_to_x_plan);
     fftw_cleanup();
     MPI_Finalize();
 
-	return 0;
+    return 0;
 }
+
